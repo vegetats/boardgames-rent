824503940d7e14113b54054c72e175528f3f16e2
vegetats <daramarotto4@gmail.com>
2025-01-23 10:53:08 -0300

    fix

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ function changeStatus(id) {
     let clickedGame = document.getElementById(`game-${id}`);
     let clickedButton = clickedGame.querySelector('.dashboard__item__button');
 
-    if(clickedButton.classList.contains('dashboard__item__button')) {
-        clickedButton.classList.toggle('dashboard__item__button__disabled')
-        clickedButton.textContent = 'Devolver'
-    } else {
-        clickedButton.classList.add('dashboard__item__button'); 
+    if(clickedButton.classList.contains('dashboard__item__button__disabled')) {
+        clickedButton.classList.remove('dashboard__item__button__disabled')
         clickedButton.textContent = 'Alugar'
+    } else { 
+        clickedButton.classList.add('dashboard__item__button__disabled')
+        clickedButton.textContent = 'Devolver'
     }
+        
 }
